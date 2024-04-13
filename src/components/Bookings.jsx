0a89497fd519770cbefa905bc502096d40032db2
iamoksanao200 <oksanao200@gmail.com>
2024-04-13 13:12:39 +0300

    start level3

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -1,13 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Search from './Search'
 import SearchResults from "./SearchResults";
 import FakeBookings from '../data/fakeBookings'
 
 const Bookings = () => {
+
   const [bookings, setBookings] = useState(FakeBookings);
+
+  // useEffect(() => {
+  //   fetch("https://cyf-react.glitch.me")
+  //   .then(response => response.json())
+  //   .then(json => setBookings(json))
+  //   .catch(error => console.log("Error fetching data:", error));
+  // }, []);
+
   const search = (searchVal) => {
     console.info('TODO!', searchVal)
   }
+
+
   return (
     <div className='App-content'>
       <div className='container' >
@@ -18,4 +29,4 @@ const Bookings = () => {
   )
 }
 
-export default Bookings
+export default Bookings;
