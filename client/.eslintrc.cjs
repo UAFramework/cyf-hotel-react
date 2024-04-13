351bc7889db8b6f0f374da65bf66c715bf2cd934
P.F. <pavel.filkovskiy@gmail.com>
2024-04-13 12:17:30 +0100

    restructure and add api server

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    // 'plugin:react/jsx-runtime',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
   ],
